b69236a0dc41382e5f720a33009985c0acd83523
New9c <ericgwhuang@gmail.com>
2025-07-10 23:37:44 +0800

    refac

--- script.ts
+++ script.ts
@@ -13,15 +13,26 @@ const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
 const demosSection = document.getElementById("demos");
 const videoBlendShapes = document.getElementById("video-blend-shapes");
 let faceLandmarker;
-let runningMode = "IMAGE";
-let enableWebcamButton;
-let streamState = false;
+const runningMode = "VIDEO";
+let streamOn = true;
 let mad = false;
 const videoWidth = 480;
 // Before we can use HandLandmarker class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment to
 // get everything needed to run.
+function startCam() {
+	// getUsermedia parameters.
+	const constraints = {
+		video: true
+	};
+	// Activate the webcam stream.
+	navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+		video.srcObject = stream;
+		video.addEventListener("loadeddata", predictWebcam);
+	});
+}
 async function createFaceLandmarker() {
+	// Create Face Landmarker
 	const filesetResolver = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm");
 	faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
 		baseOptions: {
@@ -33,42 +44,34 @@ async function createFaceLandmarker() {
 		numFaces: 1
 	});
 	demosSection.classList.remove("invisible");
+	// Start Cam
+	startCam();
 }
 createFaceLandmarker();
-/********************************************************************
-Main comtuber functionality
-********************************************************************/
+
+// Main comtuber functionality
+
 const video = document.getElementById("webcam");
+const avatar = document.getElementById("avatar");
 const canvasElement = document.getElementById("output_canvas");
 const canvasCtx = canvasElement.getContext("2d");
 // Check if webcam access is supported.
 function hasGetUserMedia() {
 	return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
-// Enable the live webcam view and start detection.
-function enableCam(event) {
-	if (!faceLandmarker) {
-		console.log("Wait! faceLandmarker not loaded yet.");
-		return;
+function toggleCam(event) {
+	streamOn = !streamOn;
+	if (streamOn){
+		startCam();
 	}
-	streamState = !streamState;
-	// getUsermedia parameters.
-	const constraints = {
-		video: true
-	};
-	// Activate the webcam stream.
-	navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-		video.srcObject = stream;
-		video.addEventListener("loadeddata", predictWebcam);
-	});
 }
-document.getElementById("avatar").addEventListener("click", function() {
+avatar.addEventListener("click", function() {
 	mad = !mad
 	console.log("Mood Swapped!");
 });
-document.getElementById("avatar").addEventListener("contextmenu", function() {
+avatar.addEventListener("contextmenu", function() {
 	event.preventDefault();
-	enableCam();
+	toggleCam();
 	console.log("Toggled Cam");
 });
 let lastVideoTime = -1;
@@ -79,18 +82,32 @@ let lastFrameTime = 0;
 const targetFPS = 10;
 const frameInterval = 1000 / targetFPS;
 
+function requestFrame() {
+	if (streamOn === true) {
+		window.requestAnimationFrame(predictWebcam);
+	} else {
+		avatar.src = "imgs/BRB.png";
+	}
+};
 async function predictWebcam(timestamp) {
 	// Time checker: only proceed if enough time has passed
 	if (timestamp - lastFrameTime < frameInterval) {
-		if (streamState === true) {
-			window.requestAnimationFrame(predictWebcam);
-		} else {
-			document.getElementById('avatar').src = "imgs/BRB.png";
-		}
+		requestFrame();
 		return;
 	}
 	console.log("Interval:", frameInterval, "Elapsed since last frame:", timestamp - lastFrameTime);
 	lastFrameTime = timestamp; // Update last frame time
+
+	let startTimeMs = performance.now();
+	if (lastVideoTime !== video.currentTime) {
+		lastVideoTime = video.currentTime;
+		results = faceLandmarker.detectForVideo(video, startTimeMs);
+	}
+	imagepicker(results.faceBlendshapes);
+	drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
+	requestFrame();
+}
+function setupCanvas(){
 	const radio = video.videoHeight / video.videoWidth;
 	video.style.width = videoWidth + "px";
 	video.style.height = videoWidth * radio + "px";
@@ -98,26 +115,7 @@ async function predictWebcam(timestamp) {
 	canvasElement.style.height = videoWidth * radio + "px";
 	canvasElement.width = video.videoWidth;
 	canvasElement.height = video.videoHeight;
-	// Now let's start detecting the stream.
-	if (runningMode === "IMAGE") {
-		runningMode = "VIDEO";
-		await faceLandmarker.setOptions({ runningMode: runningMode });
-	}
-	let startTimeMs = performance.now();
-	if (lastVideoTime !== video.currentTime) {
-		lastVideoTime = video.currentTime;
-		results = faceLandmarker.detectForVideo(video, startTimeMs);
-	}
-	imagepicker(results.faceBlendshapes);
-	// drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
-	// Call this function again to keep predicting when the browser is ready.
-	if (streamState === true) {
-		window.requestAnimationFrame(predictWebcam);
-	} else {
-		document.getElementById('avatar').src = "imgs/BRB.png";
-	}
 }
-
 function imagepicker(blendShapes) {
 	if (!blendShapes.length) {
 		return;
@@ -161,8 +159,8 @@ function imagepicker(blendShapes) {
 	}
 	document.getElementById('avatar').src = "imgs/" + (mad ? madArr[idx] : defArr[idx]);
 }
-/*
 function drawBlendShapes(el, blendShapes) {
+	setupCanvas();
 	if (!blendShapes.length) {
 		return;
 	}
@@ -178,4 +176,3 @@ function drawBlendShapes(el, blendShapes) {
 	});
 	el.innerHTML = htmlMaker;
 }
-*/
